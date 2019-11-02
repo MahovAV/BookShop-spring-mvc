e11ba3485b0025116556a4f83dc4a9a6e03eead3
user <ghrt1984@mail.ru>
2019-11-02 17:57:29 +0300

    add:JDBC Driver for MySQL & Hibernate Core

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,26 @@
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /*
-    TODO:ADD SLF4J TO PROJECT
+    todo add connection to database
+    todo add Hibernate
+    todo add Person Entity
+    todo add registration and logging
 
  */
 public class Main {
     private static Logger logger=LoggerFactory.getLogger(Main.class);
+
+    static {
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory factorySession = configuration.buildSessionFactory(builder.build());
+        logger.info("Connection SUCCESS");
+    }
 
     public static void main(String ...argc) throws Exception {
 
